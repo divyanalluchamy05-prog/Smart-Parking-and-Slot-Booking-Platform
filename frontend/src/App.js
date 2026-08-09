03ef47cd6934a8d6898171324604c723a0e6be9d
divyanalluchamy05-prog <divyanalluchamy05@gmail.com>
2026-08-09 09:55:34 +0530

    Add frontend implementation

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <div>
+      <h1>Smart Parking & Slot Booking Platform</h1>
+
+      <h2>Login</h2>
+
+      <input type="text" placeholder="Email" />
+      <br />
+      <br />
+
+      <input type="password" placeholder="Password" />
+      <br />
+      <br />
+
+      <button>Login</button>
+
+      <hr />
+
+      <h2>Dashboard</h2>
+
+      <p>Available Slots: 42</p>
+      <p>Booked Slots: 18</p>
+    </div>
+  );
+}
+
+export default App;
